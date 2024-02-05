fea4303a7f3935c6ca8b21913ad60927b0c6ad35
Bastien Ruivo <bastien.ruivo@gmail.com>
2024-02-05 09:33:58 +0100

    Update : Collision check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,38 +5,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 20f;
-    public float ZoomSpeed = 10f;
+    [Header("Speed")]
+    public float speed = 20f;
 
-    private Animator anim;
+    [Header("Camera")]
+    public Camera playerCamera;
+    public float speedZoom = 10f;
+    public float minZoom = 1f;
+    public float maxZoom = 10f;
 
-    public Camera cam;
+
+    // private variables
+    private Animator animator;
+    private Rigidbody2D rigidBody;
+
+    private enum State
+    {
+        Idle,
+        Walking
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void ZoomCamera()
+    {
+        if(playerCamera == null)
+        {
+            return;
+        }
+        float dzoom = speedZoom * Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 10.0f;
+        playerCamera.orthographicSize = Mathf.Clamp(playerCamera.orthographicSize - dzoom, minZoom, maxZoom);
+    }
+
+    private void Move()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(hAxis, vAxis, 0).normalized;
+
+        rigidBody.velocity = direction * speed * Time.deltaTime;
+
+        if(rigidBody.velocity.magnitude > 0)
+        {
+            animator.SetInteger("state", (int)State.Walking);
+            animator.SetFloat("xSpeed", rigidBody.velocity.x);
+            animator.SetFloat("ySpeed", rigidBody.velocity.y);
+        }
+        else
+        {
+            animator.SetInteger("state", (int)State.Idle);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(cam == null)
-        {
-            return;
-        }
+        ZoomCamera();        
+    }
 
-        float hAxis = Input.GetAxis("Horizontal");
-        float vAxis = Input.GetAxis("Vertical");
-
-        anim.SetFloat("xSpeed", hAxis);
-        anim.SetFloat("ySpeed", vAxis);
-
-        Vector3 movement = new Vector3(hAxis, vAxis, 0);
-        
-        // translate the player by the movement vector normalized
-        transform.Translate(movement.normalized * Speed * Time.deltaTime);
-
-        float dzoom = ZoomSpeed * Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 10.0f;
-        cam.orthographicSize = Mathf.Max(0.1f, cam.orthographicSize - dzoom);
+    private void FixedUpdate()
+    {
+        Move();
     }
 }
