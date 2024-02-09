b438af96a20419456b76ba96f13d647c5e8b488f
Bastien Ruivo <bastien.ruivo@gmail.com>
2024-02-09 14:11:37 +0100

    Update : Astar package & Guard pathfinding

--- Assets/Scripts/Enemy/AI/Node.cs
+++ Assets/Scripts/Enemy/AI/Node.cs
@@ -8,15 +8,9 @@ public class Node : MonoBehaviour
     public GameObject next;
     public GameObject previous;
     public bool isPathEnd;
-    public float tolerance = 1f;
 
     public GameObject NextTarget(bool isReversed)
     {
         return isReversed? previous : next;
-    }
-
-    public bool isInRange(Vector2 pos)
-    {
-        return Vector2.Distance(transform.position, pos) < tolerance;
     }
 }
