1365589d7e4acb1b2bcb4ea4549799ea390aa57d
Zyl0 <cyprien.tourondecourteix2@gmail.com>
2024-02-23 14:32:15 +0100

    Refactoring

--- Assets/Scripts/Systems/Ability/Abilities/AEffectVampireThirst.cs
+++ Assets/Scripts/Systems/Ability/Abilities/AEffectVampireThirst.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 namespace Systems.Ability.Abilities
 {
-    public class AVampireThirst : Ability
+    public class AEffectVampireThirst : Ability
     {
-        public AVampireThirst()
+        public AEffectVampireThirst()
         {
             //Specify if ability triggers itself on a specific context
             SelfTriggeringAbility = true; //false by default.
